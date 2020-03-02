c86d93ce4689577f22de3254990b37dc08d0c433
alifhaikal88 <alifhaikal88@gmail.com>
2020-03-02 16:23:28 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-k2uvnj
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-k2uvnj)
---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+
+import {
+  MatToolbarModule,
+  MatTableModule,
+} from '@angular/material';
+
+
+@NgModule({
+  imports:      [ BrowserModule, FormsModule,    MatToolbarModule,
+    MatTableModule, ],
+  declarations: [ AppComponent, HelloComponent ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
